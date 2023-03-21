f2a6e883e6f87c96d6ca9e9c825d76d4db1e67e6
Aaron <aaturner1995@gmail.com>
2023-03-21 12:01:28 -0300

    added classes for the shape and extended to the Circle, triangle & square. Added validate function for the inquirer

--- index.js
+++ index.js
@@ -5,11 +5,41 @@ const questions = [
     {
         type: 'input',
         name: 'text',
-        message: 'Please enter up to 3 characters:'
+        message: 'Please enter up to 3 characters:',
+        validate: function(input) {
+            if (input.length > 3) {
+                return "Please enter up to 3 characters only"
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        name: 'text-colour',
+        message: 'TEXT-COLOUR: Please enter a color keyword (OR a hexadecimal number)'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        choices: ['Circle', 'Square', 'Triangle'],
+        message: 'Please select a shape'
     },
     {
         type: 'input',
         name: 'colour',
-        message: 'Please enter '
-    }
-]
+        message: 'SHAPE-COLOUR: Please enter a color keyword (OR a hexadecimal number)'
+    } 
+];
+
+function writeToFIle(fileName, data) {
+
+};
+
+function init() {
+    inquirer.prompt(questions)
+    .then((res) => {
+        console.log(res);
+    })
+};
+
+init();
